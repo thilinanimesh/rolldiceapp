cbb3fab7b7453e246fcfc446b9760d172e101fdd
thilinanimesh <tnimesh77@gmail.com>
2024-05-08 20:37:46 +1000

    added dice assets to the project and updated pubspec.yamal file, test the code adding one image to the program (hard code), Multiple Constructor example written in the gradient_container.dart file

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rolldiceapp/custom_text.dart';
+//import 'package:rolldiceapp/custom_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -31,10 +31,18 @@ const endAlignment = Alignment.bottomRight;
 //   }
 // }
 
-
 class GradientContainer extends StatelessWidget {
   //const GradientContainer({key}):super(key: key);
-  const GradientContainer({super.key,required this.colors});//this.colorOne, this.colorTwo, {super.key,required this.colors});
+  const GradientContainer(
+      {super.key,
+      required this.colors}); //this.colorOne, this.colorTwo, {super.key,required this.colors});
+
+  //having multiple constructor example in the same class
+  const GradientContainer.purple({super.key})
+      : colors = const [
+          Color.fromARGB(255, 21, 198, 11),
+          Color.fromARGB(0, 6, 231, 148),
+        ];
 
   final List<Color> colors;
 
@@ -51,8 +59,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: CustomText("Hello World"),
+      child: Center(
+        child: Image.asset(
+          "assets/images/dice-1.png",
+          width: 200,
+        ),
       ),
     );
   }
